502916a5b11486be0beb2c753d07682d748ca816
NoraPDev <padarnori@gmail.com>
2022-10-27 19:24:23 +0000

    recipe, ingredients db tables added

--- salt_and_pepper/main/admin.py
+++ salt_and_pepper/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Recipes, Ingredients
 
 # Register your models here.
+admin.site.register(Recipes)
+admin.site.register(Ingredients)
--- salt_and_pepper/main/models.py
+++ salt_and_pepper/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ingredients(models.Model):
+    name = models.TextField()
+
+class Recipes(models.Model):
+    name = models.TextField()
+    photo = models.TextField()
+    preparation_guide = models.TextField()
+    difficulty = models.TextField()
+    ideal_for = models.TextField()
+    preparation_time = models.TextField()
