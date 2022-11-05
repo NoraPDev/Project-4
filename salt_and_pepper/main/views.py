fd553f0a19b579a6c60a2837415100b81c0f0a5d
NoraPDev <padarnori@gmail.com>
2022-11-05 12:06:06 +0000

    show recipes from db on homepage

--- salt_and_pepper/main/views.py
+++ salt_and_pepper/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from main.models import Recipe
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    context = {
+        'recipes': Recipe.objects.order_by('-id')[0:3]
+    }
+    return render(request, "home.html", context)
+
+def recipe_details(request, id):
+    context = {
+        'recipe': Recipe.objects.get(id=id)
+    }
+    return render(request, "recipe-details.html", context)
