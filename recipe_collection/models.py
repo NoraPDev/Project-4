5a7e9c885d45d5dbd631d427d85c8f6a981b2f3b
Nora Padar <Nora.Padar@smse.co.uk>
2022-11-12 14:00:48 +0000

    Recipe model field type changes

--- recipe_collection/models.py
+++ recipe_collection/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    name = models.TextField()
-    photo = models.TextField()
+    name = models.CharField(max_length=255)
+    photo = models.CharField(max_length=255)
     short_description = models.TextField(default="")
     preparation_guide = models.TextField()
-    difficulty = models.TextField()
-    ideal_for = models.TextField()
-    preparation_time = models.TextField()
-    ingredients = models.TextField()
+    difficulty = models.CharField(max_length=255)
+    ideal_for = models.CharField(max_length=255)
+    preparation_time = models.IntegerField(verbose_name="Preparation time (in minutes)")
+    ingredients = models.TextField(max_length=255)
 
     def __str__(self):
         return self.name
