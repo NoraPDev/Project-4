b8136531981c23fe7fb33e633bd5155bb1638cee
NoraPDev <padarnori@gmail.com>
2022-11-26 19:15:10 +0000

    settings for production

--- salt_and_pepper_v2/settings.py
+++ salt_and_pepper_v2/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
@@ -163,8 +163,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_URL = 'http://saltandpepper.standapp.hu/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = 'http://saltandpepper.standapp.hu/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
